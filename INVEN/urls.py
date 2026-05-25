017a5a2fd5792f1490ad36f287d42095313b7b2c
miguel <migueltorz03@gmail.com>
2026-05-25 18:56:09 -0500

    ultimo cambio 15 mayo 2025

--- INVEN/urls.py
+++ INVEN/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from apps.usuarios import views as usuario
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',usuario.Home, name="Home")
 ]
